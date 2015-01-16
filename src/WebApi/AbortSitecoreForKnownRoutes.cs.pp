fd1bac2f8d05f427a72bd737ace590430aca25fc
Serge Huijben <thehuijb@gmail.com>
2015-01-17 00:50:39 +0100

    add comments and only check against a webapi route

--- src/WebApi/AbortSitecoreForKnownRoutes.cs.pp
+++ src/WebApi/AbortSitecoreForKnownRoutes.cs.pp
@@ -4,15 +4,26 @@ using Sitecore.Pipelines.HttpRequest;
 
 namespace $rootNamespace$.WebApi
 {
+    /// <summary>
+    /// AbortSitecoreForKnownRoutes
+    /// </summary>
     public class AbortSitecoreForKnownRoutes : HttpRequestProcessor
     {
+        /// <summary>
+        /// Processes the specified args to determine if the request is a webapi request.
+        /// use this in the httprequestbegin pipeline
+        /// </summary>
+        /// <param name="args">The args.</param>
         public override void Process(HttpRequestArgs args)
         {
-            var routeCollection = RouteTable.Routes;
-            var routeData = routeCollection.GetRouteData(new HttpContextWrapper(args.Context));
-
+            // get the registered webapi route
+            var apiRoute = RouteTable.Routes["DefaultApiRoute"];
+            // check if current request is consistent with the webapi route
+            var routeData = apiRoute.GetRouteData(new HttpContextWrapper(args.Context));
+            // if not continue the pipeline
             if (routeData == null) return;
 
+            // else abort pipeline and execute the webapi call.
             HttpContext.Current.RemapHandler(routeData.RouteHandler.GetHttpHandler(HttpContext.Current.Request.RequestContext));
             args.AbortPipeline();
         }
--- src/WebApi/RegisterWebApiRoute.cs.pp
+++ src/WebApi/RegisterWebApiRoute.cs.pp
@@ -5,16 +5,26 @@ using Sitecore.Pipelines;
 
 namespace $rootNamespace$.WebApi
 {
+    /// <summary>
+    /// RegisterWebApiRoute class
+    ///
+    /// registers the route needed for the webapi to work
+    /// </summary>
     public class RegisterWebApiRoute
     {
+        /// <summary>
+        /// Processes the specified arguments and add the route.
+        /// to be used in the initialize pipeline.
+        /// </summary>
+        /// <param name="args">The pipeline arguments.</param>
         public void Process(PipelineArgs args)
         {
             var config = GlobalConfiguration.Configuration;
             config.Routes.MapHttpRoute("DefaultApiRoute",
-                                     "api/{controller}/{id}",
-                                     new {id = RouteParameter.Optional});
+                         "api/{controller}/{id}",
+                         new {id = RouteParameter.Optional});
 
-            /* Uncomment these lines to switch to JSON as the default WebApi response type */
+            /* comment out these lines to switch from JSON  to XML as the default WebApi response type */
             var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
             config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
